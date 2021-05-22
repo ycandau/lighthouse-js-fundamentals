0e1c8ed8c880cf03430b9e1e9b621b7c50a56b6d
Yves Candau <ycandau@gmail.com>
2021-05-22 08:52:33 +0000

    Add ice cream array exercise

---
+++ ice-cream.js
@@ -0,0 +1,14 @@
+const iceCreamFlavours = [
+  'chocolate',
+  'vanilla',
+  'cookies and cream',
+  'rocky road',
+  'strawberry'
+]
+
+iceCreamFlavours.push('root beer')
+
+console.log(iceCreamFlavours)
+console.log(iceCreamFlavours[0])
+console.log(iceCreamFlavours[iceCreamFlavours.length - 1])
+console.log(iceCreamFlavours.length)
